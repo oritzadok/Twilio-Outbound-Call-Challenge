eb1ffd1435d4f5271a436ade4e9bec122415852a
oritzadok <ooritzadokk@gmail.com>
2021-08-04 20:09:28 +0300

    developement: working app

--- client/src/App.js
+++ client/src/App.js
@@ -1,33 +1,83 @@
 import React, {useState, useEffect} from 'react'
+import { Device } from 'twilio-client'
+import io from 'socket.io-client'
 
 function App() {
-  const [phoneNumber, setPhoneNumber] = useState("")
+  const [number, setNumber] = useState("")
   const [callStatus, setCallStatus] = useState("")
+  const [device, setDevice] = useState(null)
+  const [busy, setBusy] = useState(false)
 
-  const call = (event) => {
+  useEffect(() => {
+    const device = new Device()
+ 
+    fetch("http://localhost:5000/token").then(
+      data => data.text()
+    ).then(
+      token => {
+        device.setup(token, {debug: true}) 
+      }
+    )
+    
+    device.on('ready', (device) => {
+      setDevice(device)
+      console.log("Twilio.Device is now ready for connections")
+    })
+    device.on('connect', connection => {
+      setBusy(true)
+      setCallStatus("")
+      console.log("connect event")
+    })
+    device.on('disconnect', () => {
+      setBusy(false)
+      console.log("disconnect event")
+    })
+    device.on('error', (error) => {
+      console.log(error.message)
+    })
 
+    const socket = io('http://localhost:5000')
+    socket.on("callStatus", data => {
+      setCallStatus(data)
+    })
+
+    return () => {
+      device.destroy()
+      setDevice(null)
+    }   
+  }, [])
+
+  /*const socket = io('http://localhost:5000')
+  socket.on("callStatus", data => {
+    setCallStatus(data)
+  })*/
+
+  const call = (e) => {
+    device.connect({number: number})
+  }
+  const hangUp = (e) => {
+    device.disconnectAll()
   }
 
   return (
     <div>
       <h1>Twilio Outbound Call</h1>
-      <form onSubmit={call}>
-        <label htmlFor="phone-number-to-call">Phone number to call:</label>
-        <br/>
-        <input
-          id="phone-number-to-call"
-          onChange={(e) => {setPhoneNumber(e.target.value)}}
-          value={phoneNumber}
-          placeholder="Enter phone number..."
-        />
-        <button> Call now! </button>
-      </form>
+      <label htmlFor="to">Phone number to call: </label>
+      <input
+        id="to"
+        onChange={(e) => {setNumber(e.target.value)}}
+        value={number}
+        placeholder="E.g. +972525553316"
+        disabled={busy}
+      />
+      <button onClick={call} disabled={busy}> Call now! </button>
+      <button onClick={hangUp} disabled={!busy}> Hang up </button>
+      <br/>
+      <br/>
       <div>
-        <label htmlFor="status">
-          Destination number: {phoneNumber}
-          <br/>
-          Phone call status:</label>
-        <h3 id="status">{callStatus}</h3>
+        <label>Call status: 
+          <b> {callStatus} </b>
+        </label>
       </div>
     </div>
   )
--- server/server.py
+++ server/server.py
@@ -1,12 +1,51 @@
 from flask import Flask
+from flask_socketio import SocketIO
+from flask import request
 from flask_cors import CORS
+from twilio.base.exceptions import TwilioRestException
+from twilio.jwt.access_token import AccessToken
+from twilio.jwt.access_token.grants import VoiceGrant
+from twilio.twiml.voice_response import VoiceResponse, Dial
+import config
 
 app = Flask(__name__)
 CORS(app)
+#app.config['SECRET_KEY'] = 'secret!'
+#socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
-@app.route("/")
-def func():
-    return {"members": ["member1", "menber2", "member3"]}
+@app.route("/token")
+def token():
+    # Create access token with credentials
+    token = AccessToken(config.account_sid, config.api_key, config.api_secret)
+
+    # Create a Voice grant and add to token
+    voice_grant = VoiceGrant(
+        outgoing_application_sid=config.TwiML_app_sid
+    )
+    token.add_grant(voice_grant)
+
+    # Return token info as JSON
+    return token.to_jwt()
+
+@app.route("/dial", methods=['POST'])
+def dial():
+    response = VoiceResponse()
+    dial = Dial(caller_id=config.my_Twilio_number)
+    dial.number(
+        request.form.get('number'),
+        status_callback_event='ringing answered completed',
+        status_callback='https://5ab34c7652e1.ngrok.io/callStatus',
+        status_callback_method='POST'
+    )
+    response.append(dial)
+    return str(response)
+
+@app.route("/callStatus", methods=['POST'])
+def status():
+    socketio.emit('callStatus', request.form.get('CallStatus'))
+    return '', 204
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
+    #app.run(debug=True)
